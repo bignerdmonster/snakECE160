3cb6019b58c420d21999f35703b2643baee2fffc
bignerdmonster <s216-felix.bryan@k12.hi.us>
2025-11-05 01:11:53 -1000

    More apples! NEver enough apples! an apple a day keeps... the professor away? technichally he is a doctor of... what IS he a doctor of? let me google this... HE GRADUATED IN 2012???? THIS GUY IS SO YOUNG!!!! anyways no clue on what his degree is on. jacinthe's theme slaps so hard...

--- snakeCore.py
+++ snakeCore.py
@@ -4,21 +4,39 @@ import random
 
 #spawns apples randomly
 class Apple:
+    appleList = []
     def __init__(self):
+        Apple.appleList.append(self)
         self.apple_size = 67
         self.x = random.randrange(0,980)
         self.y = random.randrange(0,620)
         self.color = (255,0,0)
-        self.spawn()
-    def spawn(self):
-        apple_but_rect = pg.Rect(self.x, self.y, self.apple_size, self.apple_size)
-        pg.draw.rect(screen, self.color, apple_but_rect)
+        self.apple_but_rect = pg.Rect(self.x, self.y, self.apple_size, self.apple_size)
+    
+    def spawn(self, screen):
+        pg.draw.rect(screen, self.color, self.apple_but_rect)
+    
+    @classmethod 
+    def appleBlit(cls, screen):
+        for individuApple in Apple.appleList:
+            individuApple.spawn(screen)
+
+
 
 class SnakeMat:
     def __init__(self, cols=15, rows=10):
         ### initalize the matrix. kinda duh-doy type stuff, but important regardless.
         self.center = [((cols + 1) // 2), ((rows + 1) // 2)]
-        self.mat = [[0 for place in range(cols)] for row in range(rows)]
+        self.mat = [[None for place in range(cols)] for row in range(rows)]
+    def __str__(self): # for debugging of doom and gloom
+        retStr = ""
+        for row in self.mat:
+            retStr += ' '.join([str(elem) for elem in row]) + "\n"
+        return retStr.strip()
+    # i am dizzy. i will rest now. (ai told me to rest, i am "OBEYING" LAUGHING MY ASS OFF WTF) ### jacinthe's battle theme is REALLY good. 
+    ## ok lets get real. Do I A: convert snakemat to pygame objects in the main game loop, or B: convert to printables et all in a fn., which is then called from the gameloop? I think the latter. also this theme SLAPS
+    #def pgRender(self):
+        ## ok so we want each value of the snakemat to be converted, but only valid ones. 
         
 
 class Snake:
@@ -34,10 +52,11 @@ class Snake:
             self.left = args['controls']['left']
             self.right = args['controls']['right']
         else:
-            self.up = pg.K_UP
-            self.down = pg.K_DOWN
-            self.left = pg.K_LEFT
-            self.right = pg.K_RIGHT
+            self.up = pg.K_w
+            self.down = pg.K_s
+            self.left = pg.K_a
+            self.right = pg.K_d
+
 #print(snakeMat) -- i could make this a class... ehhhhh 
             
 ## ok how to snake it --
@@ -56,25 +75,32 @@ class Snake:
 
 pg.init()
 screen = pg.display.set_mode((1080,720), pg.SCALED, vsync=1)
-
+apple = Apple()
+print("bkpoint")
 def snakeGame(menu = Menu(screenInp=screen)): ## this is the actual main game loop function!! yay
-    apple_exist = False
+    #apple_exist = False
     run = True
     while run:
         screen.fill('black')
-        if not apple_exist:
-            Apple()
-            apple_exist = True
+        ## and Corbeau's theme slaps too zawg.
         for event in pg.event.get():
             if event.type == pg.QUIT:
-                pg.exit()
-                quit(0)
+                pg.quit()
+                exit(0)
         keys = pg.key.get_pressed()
-        
         if keys[pg.K_w]:
-            screen.fill('yellow')
+            Apple()
+        if keys[pg.K_s]:
+            Apple.appleList.clear()
         if keys[pg.K_ESCAPE]:
             run=False
+        #if not apple_exist:
+        #    Apple()
+        #    apple_exist = True 
+        Apple.appleBlit(screen)
+       
+        
+        
         pg.display.flip()
         clock.tick(framerate)
     menu.notstop = True
